5b4cc6890fe963b702ff7cc2103d90de0dfc2487
Zondeka Solomon Motsumi <motsumisolomon@gmail.com>
2023-12-11 15:51:50 +0200

    fixed login and sign up validation.

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProfileView from "../ProfileView/ProfileView";
+
+const SideBar = () => {
+
+    return(
+        <div className="sidebar">
+            <ProfileView />
+
+        </div>
+    )
+}
+export default SideBar;
